fe8bcc43be7bc68a5f275f7899e2b1ce7171c3bd
Aubrey Neimeier <neimeier.1@osu.edu>
2018-08-31 19:28:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getRespositories(){
   req.send();
 }
 
-//each repository should call on getCommits on click. 
+//each repository should call on getCommits on click.
 function displayRepositories(event, data){
   var repos = JSON.parse(this.responseText);
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
@@ -15,7 +15,7 @@ function displayRepositories(event, data){
 
 //make a request for commits on the repository that was clicked
 function getCommits(){
-  
+
 }
 
 
@@ -23,18 +23,18 @@ function getCommits(){
 //The display should include the author's github name, the authors full name and the commit message
 // give the link data attributes of username and repository to be used by the getCommits function
 function displayCommits(){
-  
+
 }
 
 // make a request for the branches of the repo
 function getBranches(){
 
-  
+
 }
 
 
-// should fill the details div with a list of names of each branch of the repository. 
+// should fill the details div with a list of names of each branch of the repository.
 // the link attributes of username and repository for use by getBranches()
 function displayBranches(){
-  
+
 }
